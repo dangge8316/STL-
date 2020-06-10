f2e14790d70bbc22631fc858ddee1e370dbc8556
dangge8316 <17972819@qq.com>
2020-06-10 11:49:16 +0800

    添加项目文件。

---
+++ STL容器项目练习.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "sizeFilter.hpp"
+
+int main()
+{
+	sizeFilter<int,multiset<int>> sf;
+
+	for (int i = 0; i < 10; i++)
+	{
+		sf.insert(i * 4);
+	}
+	sf.erase(0);
+	sf.erase(36);
+
+	sf.getMax();
+	sf.getMin();
+}
+
---
+++ sizeFilter.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+template <class _Ty,
+	class _Container = set<_Ty> >
+	class sizeFilter
+{
+public:
+	typedef sizeFilter<_Ty, _Container> myT;
+	typedef typename _Container::size_type size_type;
+	typedef typename _Container::value_type value_type;
+	//默认构造函数
+	sizeFilter() :c()
+	{
+		
+	}
+	//使用指定sizeFilter容器构造
+	sizeFilter(const myT& _Right) :c(_Right.c)
+	{
+
+	}
+	//使用指定容器类型构造
+	sizeFilter(const _Container& cont) :c(cont)
+	{
+
+	}
+	//赋值运算符重载
+	myT& operator=(const myT& other)
+	{
+		c = other.c;
+		return (*this);
+	}
+	//获取容器大小
+	size_type size() const
+	{
+		return c.size();
+	}
+	//判断容器是否为空
+	bool empty() const
+	{
+		return c.empty();
+	}
+	//插入元素
+	bool insert(const value_type& element)
+	{
+		typename _Container::iterator cit = c.insert(c.begin(), element);
+		if (cit != c.end())
+		{
+			cout << "插入" << element << "成功!" << endl;
+			return true;
+		}
+		cout << "插入数据失败!";
+		return false;
+	}
+	//删除元素
+	bool erase(const value_type& element)
+	{
+		if (c.erase(element) > 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	//获取最大值
+	pair<value_type, bool> getMax()
+	{
+		pair<value_type, bool> ret;
+		if (c.size() > 0)
+		{
+			ret.first = *(--(c.end()));
+			ret.second = true;
+			cout << "该容器中最大值为:" << ret.first << endl;
+			return ret;
+		}
+		ret.second = false;
+		cout << "获取最大值失败,该容器中没有任何数据!" << endl;
+		return ret;
+	}
+	//获取最小值
+	pair<value_type, bool> getMin()
+	{
+		pair<value_type, bool> ret;
+		if (c.size() > 0)
+		{
+			ret.first = *(c.begin());
+			ret.second = true;
+			cout << "该容器中最小值为:" << ret.first << endl;
+			return ret;
+		}
+		ret.second = false;
+		cout << "获取最小值失败,该容器中没有任何数据!" << endl;
+		return ret;
+	}
+
+protected:
+	_Container c;
+};
